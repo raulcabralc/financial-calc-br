d3d763be6e9413797921ea02ebf4796728529407
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-10-01 11:58:33 -0300

    chore(dist): delete dist from repository

--- dist/calculators/creditCard.d.ts
+++
@@ -1,5 +0,0 @@
-import { creditCardPaga, creditCard } from "../types/types";
-declare class creditCardCalc {
-    calcRotativo(valorFatura: number, valorPago: number, taxaMensal?: number): creditCard | creditCardPaga;
-}
-export default creditCardCalc;
